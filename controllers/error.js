750eec1b9d9060eb34c7a7c6aba06f33bd706b5a
GhostITShell <56477309+Subject2501@users.noreply.github.com>
2022-07-24 12:31:51 +0300

    sytled

--- controllers/error.js
+++ controllers/error.js
@@ -2,7 +2,6 @@ module.exports.get404Page = (req, res) => {
     res.status(404).render('errors/404', { title: 'Sayfa Bulunamadı!' });
 }
 
-
 module.exports.get500Page = (req, res) => {
     res.status(500).render('errors/500', { title: 'Hata!' });
 } 
