51e781a02668ecceb0a9c52c9c230cfdbd654081
Buğra Yüksel <109875603+BugraYukselMD@users.noreply.github.com>
2022-09-23 17:08:19 +0300

    Update public.js

--- routers/public.js
+++ routers/public.js
@@ -12,6 +12,5 @@ router.get('/linktree', locals, controller.getLinkTree);
 
 // for -post functions
 router.post('/list-by-category', locals, controller.postList);
-router.post('/load-more/:blogslength', locals, controller.postLoadMore);
 
 module.exports = router;
